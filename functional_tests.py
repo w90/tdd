dcea299df7be56b5d53d1f9c224c16513da0c378
leo <leszek.rutkowski@gmail.com>
2017-05-19 22:11:40 +0200

    Zmiany w tescie funkcjonalnym - dodanie metody pomocniczej check_for_row_in_list_table

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # Metoda pomocnicza, bez 'test' na początku
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     # Edyta dowiedziała się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia.
     # Postanowiła więc przejść na stronę główną tej aplikacji.
 
@@ -35,10 +43,11 @@ class NewVisitorTest(unittest.TestCase):
 # Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla
 # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = self.browser.find_elements_by_tag_name('tr')
+        # self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
 
 # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
 # Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
@@ -50,12 +59,15 @@ class NewVisitorTest(unittest.TestCase):
 
 # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
-        self.assertIn(
-            '2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Kupić pawie pióra')
+        self.check_for_row_in_list_table('2: Użyć pawich piór do zrobienia przynęty')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
+        # self.assertIn(
+        #     '2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows]
+        # )
 
 # Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej
 # unikatowy adres URL, obok którego znajduje się pewien tekst z wyjaśnieniem
